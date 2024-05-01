6d6de359daa023a6a0d3ed33770fde5ef480d543
djangouser <knz2374466@stu.o-hara.ac.jp>
2024-05-01 10:32:14 +0900

    新規作成

---
+++ src/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,43 @@
+package scoremanager.main;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectListAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		//ローカル変数の宣言 1
+		HttpSession session = req.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");//ログインユーザー
+
+
+		List<Subject> subjects = null;// 学生リスト
+		SubjectDao sDao = new SubjectDao();//学生Dao
+
+		//DBからデータ取得 3
+
+		subjects = sDao.filter(teacher.getSchool());
+
+		//DBへデータ保存 5
+		//なし
+
+		// リクエストに学生リストをセット
+		req.setAttribute("subjects", subjects);
+
+		//JSPへフォワード 7
+		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
+	}
+
+
+
+}
